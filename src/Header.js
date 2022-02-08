39b25f53fd9eea58f6767410d757fb1abdd6cbbf
Deepanshu Singh <singhdepanshu786@gmail.com>
2022-02-09 00:23:17 +0530

    Initial amazon first commit

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <nav className='Header'>
+            <img
+                className='header_logo'
+                src="https://banner2.cleanpng.com/20180824/zyx/kisspng-amazon-com-logo-amazon-prime-video-berlin-font-5b7f9de40422d8.9445487015350901480169.jpg">
+            </img>
+        </nav>
+    )
+
+}
+
+export default Header;
+ 
